abda0e07e2e470d9333ac79691fb8e247192576f
Mario Noseda <mnoseda@bluewin.ch>
2018-04-20 12:39:11 +0200

    further hw testing

--- clusterwink_mk_ii/main.c
+++ clusterwink_mk_ii/main.c
@@ -12,18 +12,31 @@
 int main(void)
 {
     uint8_t u8Duty = 0;
+	uint16_t i;
 	
 	portInit();
 	initPWM(0);
 	startPWM();
-	enablePLED();
+	//enablePLED();
+	enableAudio();
+	
+// 	for(i=0;i<4;i++)
+// 	{
+// 		PORT_VOL |= (1<<VOL_UD);
+// 		DDR_VOL |= (1<<VOL_UD);
+// 		wait_1ms(1);
+// 		
+// 		DDR_VOL &= ~(1<<VOL_UD);
+// 		PORT_VOL &= ~(1<<VOL_UD);
+// 		wait_1ms(1);
+// 	}
 	
     while (1) 
     {
-		setDuty(0);
-		wait_1ms(1000);
-		setDuty(1);
-		wait_1ms(1000);
+// 		setDuty(0);
+// 		wait_1ms(1000);
+// 		setDuty(1);
+// 		wait_1ms(1000);
     }
 }
 
